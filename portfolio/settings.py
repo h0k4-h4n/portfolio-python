2d72f1a3eedf7c7960372559eb6f1ef84336f7f3
Otto <ottoalves22@gmail.com>
2020-03-04 13:46:19 -0300

    bootstrap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'jobs',
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
