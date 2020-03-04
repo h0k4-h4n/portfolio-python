581bb9e74729b0b521f80818749b5a67ffe1b148
Otto <ottoalves22@gmail.com>
2020-03-04 17:23:06 -0300

    Blog feito

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pud_date = models.DateTimeField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now(), blank=True)
     body = models.TextField()
     imagem = models.ImageField(upload_to='images/')
