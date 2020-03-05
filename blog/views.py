22cb2810dbcaf5e089ee00b8e64e2f835b2e425d
Otto <ottoalves22@gmail.com>
2020-03-05 14:17:44 -0300

    Agora /blog/id funciona

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
+
 def allblogs(request):
     blog = Blog.objects.all()
     return render(request, 'blog/allblogs.html', {'blog': blog})
+
+def detail(request, blog_id):
+    detailblog = Blog.objects.get(pk=blog_id)
+    print("detailblog")
+    return render(request, 'blog/detail.html', {'blog': detailblog})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'otto',
+        'USER': 'postgres',
         'PASSWORD': '123Mudar@',
         'HOST': '127.0.0.1',
         'PORT': '5432',
